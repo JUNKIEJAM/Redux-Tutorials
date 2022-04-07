4aab757bb5b77e3c51a66899154e607f2751c64b
JUNKIEJAM <66085589+JUNKIEJAM@users.noreply.github.com>
2022-04-07 09:45:01 +0530

    buttons

---
+++ src/components/Shop.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Shop() {
+  return (
+    <div>
+        <h2>Buy here </h2>
+<button className="btn btn-primary mx-2">
+    +
+</button>
+Add this item to cart this item
+<button className="btn btn-primary mx-2">
+    -
+</button>
+    </div>
+  )
+}
+
+export default Shop
